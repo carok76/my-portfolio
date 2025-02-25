3ed6dcdb21b8348c562af5b34ab25d562daddd4b
carok76 <carolin.krahmer@gmail.com>
2025-02-25 08:44:27 +0100

    Update script.js

--- src/script.js
+++ src/script.js
@@ -1,71 +1,27 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const picWeather = document.getElementById("pic-weather");
+  const images = [
+    { id: "pic-weather", colorSrc: "src/weather-app-colour.png", bwSrc: "src/weather-app-bw.png" },
+    { id: "pic-weather-mobile", colorSrc: "src/weather-app-colour.png", bwSrc: "src/weather-app-bw.png" },
+    { id: "pic-poem", colorSrc: "src/poem-generator-colour.png", bwSrc: "src/poem-generator-bw.png" },
+    { id: "pic-poem-mobile", colorSrc: "src/poem-generator-colour.png", bwSrc: "src/poem-generator-bw.png" },
+    { id: "pic-recipe", colorSrc: "src/recipe-generator-colour.png", bwSrc: "src/recipe-generator-bw.png" },
+    { id: "pic-recipe-mobile", colorSrc: "src/recipe-generator-colour.png", bwSrc: "src/recipe-generator-bw.png" },
+    { id: "pic-index", colorSrc: "src/Caro_colour_1.jpg", bwSrc: "src/Caro_bw_1.jpg" },
+    { id: "pic-about", colorSrc: "src/Caro_colour_3.jpg", bwSrc: "src/Caro_bw_3.jpg" }
+  ];
 
-  picWeather.addEventListener("mouseenter", function () {
-    picWeather.src = "./src/weather-app-colour.png";
-  });
+  images.forEach(img => {
+    const element = document.getElementById(img.id);
+    if (element) {
 
-  picWeather.addEventListener("mouseleave", function () {
-    picWeather.src = "./src/weather-app-bw.png";
+      element.addEventListener("mouseenter", function () {
+        this.src = img.colorSrc;
+      });
+
+      element.addEventListener("mouseleave", function () {
+        this.src = img.bwSrc;
+      });
+    } else {
+    }
   });
 });
-
-document.addEventListener("DOMContentLoaded", function () {
-    const picWeather = document.getElementById("pic-weather-mobile");
-  
-    picWeather.addEventListener("mouseenter", function () {
-      picWeather.src = "./src/weather-app-colour.png";
-    });
-  
-    picWeather.addEventListener("mouseleave", function () {
-      picWeather.src = "./src/weather-app-bw.png";
-    });
-  });
-
-document.addEventListener("DOMContentLoaded", function () {
-    const picWeather = document.getElementById("pic-poem");
-  
-    picWeather.addEventListener("mouseenter", function () {
-      picWeather.src = "./src/poem-generator-colour.png";
-    });
-  
-    picWeather.addEventListener("mouseleave", function () {
-      picWeather.src = "./src/poem-generator-bw.png";
-    });
-  });
-
-  document.addEventListener("DOMContentLoaded", function () {
-    const picWeather = document.getElementById("pic-poem-mobile");
-  
-    picWeather.addEventListener("mouseenter", function () {
-      picWeather.src = "./src/poem-generator-colour.png";
-    });
-  
-    picWeather.addEventListener("mouseleave", function () {
-      picWeather.src = "./src/poem-generator-bw.png";
-    });
-  });
-
-  document.addEventListener("DOMContentLoaded", function () {
-    const picWeather = document.getElementById("pic-recipe");
-  
-    picWeather.addEventListener("mouseenter", function () {
-      picWeather.src = "./src/recipe-generator-colour.png";
-    });
-  
-    picWeather.addEventListener("mouseleave", function () {
-      picWeather.src = "./src/recipe-generator-bw.png";
-    });
-  });
-
-  document.addEventListener("DOMContentLoaded", function () {
-    const picWeather = document.getElementById("pic-recipe-mobile");
-  
-    picWeather.addEventListener("mouseenter", function () {
-      picWeather.src = "./src/recipe-generator-colour.png";
-    });
-  
-    picWeather.addEventListener("mouseleave", function () {
-      picWeather.src = "./src/recipe-generator-bw.png";
-    });
-  });
