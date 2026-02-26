1ddb474f4de6c45c5b9bda7e7b5c23ea82a5bdd1
carok76 <carolin.krahmer@gmail.com>
2026-02-26 11:46:46 +0100

    Updated Work section and mobile improvement

--- src/script.js
+++ src/script.js
@@ -7,8 +7,15 @@ document.addEventListener("DOMContentLoaded", function () {
     { id: "pic-recipe", colorSrc: "src/recipe-generator-colour.png", bwSrc: "src/recipe-generator-bw.png" },
     { id: "pic-recipe-mobile", colorSrc: "src/recipe-generator-colour.png", bwSrc: "src/recipe-generator-bw.png" },
     { id: "pic-react", colorSrc: "src/react-weather-app-color.png", bwSrc: "src/react-weather-app-bw.png"},
+    { id: "pic-about-mobile", colorSrc: "src/Caro_colour_3.jpg", bwSrc: "src/Caro_bw_3.jpg" },
     { id: "pic-index", colorSrc: "src/Caro_colour_1.jpg", bwSrc: "src/Caro_bw_1.jpg" },
-    { id: "pic-about", colorSrc: "src/Caro_colour_3.jpg", bwSrc: "src/Caro_bw_3.jpg" }
+    { id: "pic-about", colorSrc: "src/Caro_colour_3.jpg", bwSrc: "src/Caro_bw_3.jpg" },
+    { id: "pic-clock", colorSrc: "src/world-clock-app-colour.png", bwSrc: "src/world-clock-app-bw.png" },
+    { id: "pic-clock-mobile", colorSrc: "src/world-clock-app-colour.png", bwSrc: "src/world-clock-app-bw.png" },
+    { id: "pic-travel", colorSrc: "src/travel-page-colour.png", bwSrc: "src/travel-page-bw.png" },
+    { id: "pic-travel-mobile", colorSrc: "src/travel-page-colour.png", bwSrc: "src/travel-page-bw.png" },
+    { id: "pic-dictionary", colorSrc: "src/dictionary-colour.png", bwSrc: "src/dictionary-bw.png" },
+    { id: "pic-dictionary-mobile", colorSrc: "src/dictionary-colour.png", bwSrc: "src/dictionary-bw.png" }
   ];
 
   images.forEach(img => {
